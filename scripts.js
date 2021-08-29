b63d7a4b0ce685bd01505c1ae69e2fb76d70675a
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-08-29 18:18:55 -0300

    iniciando implementação da lógica

--- scripts.js
+++ scripts.js
@@ -1,19 +1,121 @@
 const Modal = {
   openOrClose() {
-    // abrir ou fechar Modal --> adiciona ou remove a class active do Modal
     document.querySelector(".modal-overlay").classList.toggle("active");
   },
 };
 
-/*
-const Modal = {
-  open() {
-    // abrir Modal --> adicionar a class active ao Modal
-    document.querySelector(".modal-overlay").classList.add("active");
+const transactions = [
+  {
+    description: "Luz",
+    amount: -50000,
+    date: "23/01/2021",
   },
-  close() {
-    // fechar o modal --> remover a class active do Modal
-    document.querySelector(".modal-overlay").classList.remove("active");
+  {
+    description: "WebSite",
+    amount: 500000,
+    date: "23/01/2021",
+  },
+  {
+    description: "Internet",
+    amount: -20000,
+    date: "23/01/2021",
+  },
+];
+
+const Transaction = {
+  all: transactions,
+  add(transaction) {
+    Transaction.all.push(transaction);
+    App.reload();
+  },
+  remove(index) {
+    Transaction.all.splice(index, 1);
+    App.reload();
+  },
+  incomes() {
+    let income = 0;
+    Transaction.all.forEach((transaction) => {
+      if (transaction.amount > 0) income += transaction.amount;
+    });
+    return income;
+  },
+  expenses() {
+    let expense = 0;
+    Transaction.all.forEach((transaction) => {
+      if (transaction.amount < 0) expense += transaction.amount;
+    });
+    return expense;
+  },
+  total() {
+    return Transaction.incomes() + Transaction.expenses();
   },
 };
-*/
+
+const DOM = {
+  transactionsContainer: document.querySelector("#data-table tbody"),
+  addTransaction(transaction, index) {
+    const tr = document.createElement("tr");
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+
+    DOM.transactionsContainer.appendChild(tr);
+  },
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? "income" : "expense";
+    const amount = Utils.formatCurrency(transaction.amount);
+
+    const html = `
+      <td class="description">${transaction.description}</td>
+      <td class="${CSSclass}">${amount}</td>
+      <td class="date">${transaction.date}</td>
+      <td>
+        <img src="./assets/minus.svg" alt="Remover transação" />
+      </td>
+    `;
+    return html;
+  },
+  updateBalance() {
+    document.querySelector("#incomeDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.incomes(),
+    );
+    document.querySelector("#expenseDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.expenses(),
+    );
+    document.querySelector("#totalDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.total(),
+    );
+  },
+  clearTransactions() {
+    DOM.transactionsContainer.innerHTML = "";
+  },
+};
+
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? "-" : "";
+    value = String(value).replace(/\D/g, "");
+    value = Number(value) / 100;
+
+    value = value.toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    });
+
+    return signal + value;
+  },
+};
+
+const App = {
+  init() {
+    Transaction.all.forEach((transaction) => {
+      DOM.addTransaction(transaction);
+    });
+
+    DOM.updateBalance();
+  },
+  reload() {
+    DOM.clearTransactions();
+    App.init();
+  },
+};
+
+App.init();
